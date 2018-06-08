2c2490ddad6288364fac77b1f6dad46524063295
Diego Alvarez <daalvarez@unbosque.edu.co>
2018-06-08 12:26:47 -0500

    Bootstrap admin and fan users

--- grails-app/init/co/com/mcu/BootStrap.groovy
+++ grails-app/init/co/com/mcu/BootStrap.groovy
@@ -3,6 +3,20 @@ package co.com.mcu
 class BootStrap {
 
     def init = { servletContext ->
+
+    def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+    def adminUser = new User(username: 'admin', password: 'admin').save()
+    UserRole.create adminUser, adminRole
+
+    def fanRole = new Role(authority: 'ROLE_FAN').save()
+    def fanUser = new User(username: '1', password: '1').save()
+    UserRole.create fanUser, fanRole
+
+    UserRole.withSession {
+      it.flush()
+      it.clear()
+    }
+    
     }
     def destroy = {
     }
